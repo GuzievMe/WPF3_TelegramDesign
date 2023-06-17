35ae05ca8da78f84f924b47e16812b953cb7e940
jeqiMaham <mahamguziyev@gmail.com>
2023-06-17 13:01:11 -0700

    first

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfPrcts
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void txtBox_GotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            if (sender is TextBox tb)
+            {
+                tb.Text = "";
+                tb.Foreground = Brushes.Black;
+            }
+        }
+
+        private void txtBox_LostKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            if (sender is TextBox tb)
+            {
+                if (string.IsNullOrWhiteSpace(tb.Text))
+                {
+                    if (tb.Name == "txtBox")
+                    {
+                        tb.Text = "Arka plan metni";
+                        tb.Foreground = Brushes.Gray; return;
+                    }
+                    tb.Text = "Search";
+                    tb.Foreground = Brushes.Gray;
+
+                }
+            }
+        }
+
+
+    }
+}
